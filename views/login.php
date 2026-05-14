715a88f7a425a913497ab8fce2e5e7b1fef03110
Prazaa <maulanapraza310306@gmail.com>
2026-05-14 15:48:07 +0700

    Update login.php

--- views/login.php
+++ views/login.php
@@ -6,17 +6,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $query = "SELECT * FROM users WHERE username = '$username'";
-    $result = mysqli_query($conn, $query);
-    if ($result && mysqli_num_rows($result) > 0) {
-        $user = mysqli_fetch_assoc($result);
+    // Gunakan Prepared Statement agar aman dan stabil di server Linux
+    $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE username = ?");
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if ($user = mysqli_fetch_assoc($result)) {
+        // password_verify akan membandingkan input '2403125938' dengan hash di DB
         if (password_verify($password, $user['password'])) {
             $_SESSION['user'] = $user['username'];
             $_SESSION['role'] = $user['role'];
             header('Location: ../index.php');
             exit();
         } else {
-            $error = "Password salah!";
+            $error = "Password salah! (Pastikan data di DB sudah di-hash)";
         }
     } else {
         $error = "Username tidak ditemukan!";
